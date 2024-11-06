bf387d6c6a8be1364fdde4980aafb84897b6f999
WalterViegener <walterviegener@yahoo.de>
2024-11-06 11:54:34 +0100

    Update ServerSession.cs

--- MqUtil/Base/ServerSession.cs
+++ MqUtil/Base/ServerSession.cs
@@ -117,8 +117,15 @@ namespace MqUtil.Base{
 					return $"Cannot delete folder {infoFolder}. Please make sure no other processes are accessing it.";
 				}
 			}
-			if (!FileUtils.IsUnix() && !MqUtil.Util.Utils.XgboostDllRunsFine()){
-				return MqUtil.Util.Utils.GetXgboostMessage();
+			if (!FileUtils.IsUnix()){
+				try {
+					MqUtil.Util.Utils.XgboostDllRunsFine();
+				}
+				catch {
+					return MqUtil.Util.Utils.GetXgboostMessage();
+
+
+				}
 			}
 			if (!string.IsNullOrEmpty(infoFolder)){
 				Directory.CreateDirectory(infoFolder);
