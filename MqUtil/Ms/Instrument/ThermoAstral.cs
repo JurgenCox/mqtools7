8e73b0f1ba82107cd04e99cb7da09369c86b34a7
WalterViegener <walterviegener@yahoo.de>
2025-07-16 18:04:52 +0200

    new MassTolPpmDefault for Astral

--- MqUtil/Ms/Instrument/ThermoAstral.cs
+++ MqUtil/Ms/Instrument/ThermoAstral.cs
@@ -4,8 +4,8 @@ namespace MqUtil.Ms.Instrument{
 		public ThermoAstral(int index) : base(index){
 		}
 		public override string Name => "Astral";
-		public override double DiaInitialPrecMassTolPpmDefault => 5;
-		public override double DiaInitialFragMassTolPpmDefault => 20;
+		public override double DiaInitialPrecMassTolPpmDefault => 4;
+		public override double DiaInitialFragMassTolPpmDefault => 10;
 		public override DiaQuantMethod DiaQuantMethodDefault => DiaQuantMethod.TopFragmentsAnnotLfqSplit;
 		public override bool UseMs1CentroidsDefault => true;
 	}
