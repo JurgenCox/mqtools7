f1728b42973b293ced7d61f4c1697ab528dc54c2
juergen <cox@biochem.mpg.de>
2025-08-14 02:31:47 +0200

    removing interfaces

--- MqUtil/Base/IFileIndexProvider.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace MqUtil.Base {
-	public interface IFileIndexProvider {
-		HashSet<int> GetFileIndices(string exp);
-		HashSet<int> GetFileIndicesNoPtm(string exp);
-	}
-}
--- MqUtil/Base/IRtAlignmentViewerData.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace MqUtil.Base
-{
-	public interface IRtAlignmentViewerData : IChromatogramData {
-		Tuple<double[], double[]> GetRtCalibrationFunction(int i, bool positive);
-	}
-}
