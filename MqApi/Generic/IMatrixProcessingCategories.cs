0d61be1e24eaef853fff96486ceeae5716d6780f
juergen <cox@biochem.mpg.de>
2026-01-22 11:22:07 +0100

    Slight renaming of processing categories

--- MqApi/Generic/IMatrixProcessingCategories.cs
+++ MqApi/Generic/IMatrixProcessingCategories.cs
@@ -1,21 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MqApi.Generic
+﻿namespace MqApi.Generic
 {
-    public static class IMatrixProcessingCategories {
-		
+	public static class IMatrixProcessingCategories
+	{
 		public const string DataHandling = "Data handling";
-		public const string DataNormalization = "Data normalization";
-        public const string DataAnnotation = "Data annotation";
-        public const string StatisticalAnalysis = "Statistical analysis";
+		public const string DataNormalization = "Normalization";
+		public const string DataAnnotation = "Annotation";
+		public const string StatisticalAnalysis = "Statistical analysis";
 		public const string DataVisualization = "Data visualization";
 		public const string Plugins = "Plugins";
-
-
-
-    }
+	}
 }
